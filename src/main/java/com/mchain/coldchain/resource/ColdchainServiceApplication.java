f49ed1cfbe0d810673a912b99393419202aa7c34
renxiaoqing <renxiaoqing@mingchaintech.com>
2019-08-19 16:25:16 +0800

    push at datetime

---
+++ src/main/java/com/mchain/coldchain/resource/ColdchainServiceApplication.java
@@ -0,0 +1,13 @@
+package null;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColdchainServiceApplication 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(ColdchainServiceApplication.class, args);
+    }
+}
